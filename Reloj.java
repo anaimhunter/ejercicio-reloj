bc7ea885c52c9e53d673840f56a1944ee721690e
Ana M.Hunter <anaimhunter@hotmail.com>
2017-08-30 15:22:20 -0500

    Muestra el reloj de cuatro digitos y la Documentacion

--- Reloj.java
+++ Reloj.java
@@ -1,28 +1,47 @@
 
-
+/**
+ * La clase Reloj reprsenta un objeto reloj con horas y minutos
+ * @author Ana M.Hunter
+ */
 public class Reloj
 {
    
     private int horas;
     private int minutos;
-
+    /**
+     * Constructor que inicializa las horas y minutos
+     * @param horaInicial Representa la hora en la que se inicia el reloj
+     * @param minInicial Representa los minutos en los que inicia el reloj
+     */
     public Reloj(int horaInicial, int minInicial)
     {
         // initialise instance variables
         horas=horaInicial;
         minutos=minInicial;
     }
-    
+    /**
+     * Da el tiempo del reloj
+     */
     public String dimeTuTiempo()
     {
         String cadResultado="";
+        if(horas<10)
+        {
+            cadResultado += "0";
+        }
         cadResultado +=horas;
         cadResultado=cadResultado +":";
+        if(minutos<10)
+        {
+            cadResultado += "0";
+        }
         cadResultado+=minutos;
         return cadResultado;
         
     }
-    
+    /**
+     * Avanza las horas y minutos del reloj 
+     */
     public void ticTac()
     {
         if(minutos<60)
